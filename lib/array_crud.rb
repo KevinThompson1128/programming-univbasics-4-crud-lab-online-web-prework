2b7f7263712491874be775e154c7b4eef01a67d0
Kevin Thompson <indelible11@yahoo.com>
2019-10-21 05:39:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  create_an_array << "new baby"
+  add_element_to_end_of_array << "new baby"
 end
 
 def add_element_to_start_of_array(array, element)
