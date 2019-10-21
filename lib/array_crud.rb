465dc22d223af42d08ef957d2ba62b3ba2c0ac14
Kevin Thompson <indelible11@yahoo.com>
2019-10-21 05:08:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,39 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  create_an_array = ["Kevin", "Danay", "Ariah" "Clarence"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  create_an_array << "new baby"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  my_family.unshift("mom inlaw")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  my_family.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  my_family.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  my_family[1]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  my_family[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  my_family[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  my_family[0] = "husband"
 end
