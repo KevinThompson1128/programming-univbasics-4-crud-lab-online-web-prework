dd8801fdad6cb6f3e5ace8c9cde9532a92336cc7
Kevin Thompson <indelible11@yahoo.com>
2019-10-21 05:46:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  create_an_array = ["wow", "I", "am", "really", "learning"]
+  create_an_array = ["Kevin", "Danay", "Ariah", "Clarence"]
 end
 
 def add_element_to_end_of_array(array, element)
-  add_element_to_end_of_array << "arrays"
+  create_an_array << "new baby"
 end
 
 def add_element_to_start_of_array(array, element)
@@ -19,7 +19,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  remove_element_from_start_of_array.shift
+  my_family.shift
 end
 
 def retrieve_element_from_index(array, index_number)
